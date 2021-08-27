3ecd3173b571cc4286a10fec4a85d81075547f56
Documenter.jl <documenter@juliadocs.github.io>
2021-08-27 15:11:48 +0000

    build based on 7981b45

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Introduction","title":"Introduction","text":"<img style=\"height:2em;\" alt=\"NuFlux.jl\" src=\"docs/src/assets/nuflux.svg\"/>","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"Neutrino flux table parser","category":"page"}]
+}
