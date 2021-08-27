38905eeaf68d2c20a44beb508e8c4045053e02dd
Johannes Schumann <johannes.schumann@fau.de>
2021-08-27 16:40:34 +0200

    Add make for docs

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter, NuFlux
+
+makedocs(;
+    modules = [NuFlux],
+    authors = "Johannes Schumann",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+    ),
+    pages=[
+        "Introduction" => "index.md",
+    ],
+    repo="https://github.com/8me/NuFlux.jl/blob/{commit}{path}#L{line}",
+    sitename="NuFlux.jl",
+)
+
+deploydocs(;
+    repo="github.com/KM3NeT/NuFlux.jl",
+)
