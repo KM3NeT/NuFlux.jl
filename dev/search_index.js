17f63b91fcb9f258228ed012463bd508c2fb6693
Documenter.jl <documenter@juliadocs.github.io>
2021-08-27 17:48:29 +0000

    build based on f4112c0

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Introduction","title":"Introduction","text":"<img style=\"height:2em;\" alt=\"NuFlux.jl\" src=\"https://github.com/8me/NuFlux.jl/raw/main/docs/src/assets/nuflux.svg\"/>","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"Neutrino flux table parser","category":"page"}]
+[{"location":"","page":"Introduction","title":"Introduction","text":"<img style=\"height:2em;\" alt=\"NuFlux.jl\" src=\"https://github.com/8me/NuFlux.jl/raw/main/docs/src/assets/nuflux.svg\"/>","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"(Image: Dev) (Image: Build Status)","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"Neutrino flux table parser","category":"page"}]
 }
