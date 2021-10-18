819f649dd949f21f729e22eb9839eb674ab1222e
Johannes Schumann <johannes.schumann@fau.de>
2021-10-18 10:38:36 +0200

    Add first tests for power law fluxes

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,13 @@ const DATA_DIR = joinpath(@__DIR__, "../data")
     @test !isa(try NuFlux.readfluxfile(joinpath(DATA_DIR, "frj-ally-20-12-solmin.d")) catch e e end, Exception)
 end
 
+@testset "power law flux" begin
+    f = NuFlux.PowerLawFlux(-2)
+    @test 1e-12 ≈ NuFlux.flux(f, 1e2)
+end
+
+@testset "power law flux w. cutoff" begin
+    f = NuFlux.PowerLawFlux(-2, 1, 3e6)
+    @test 1e-12 ≈ NuFlux.flux(f, 1e2) atol=1e-15
+    @test 7.165e-21 ≈ NuFlux.flux(f, 1e6) atol=1e-24
+end
