cd72c5263c1994b9ac9a3d09ebfe039640e32a61
Santiago Pena Martinez <spenamartinez@km3net.de>
2025-04-03 15:51:14 +0200

    Fix floating point error and include test unit

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,7 @@ const DATA_DIR = joinpath(@__DIR__, "../data")
 
 @testset "load fluxtable" begin
     @test !isa(try NuFlux.readfluxfile(joinpath(DATA_DIR, "frj-ally-20-12-solmin.d")) catch e e end, Exception)
+    flux_table = NuFlux.readfluxfile(joinpath(DATA_DIR, "frj-ally-20-12-solmin.d"))
+    @test !isa(try NuFlux.flux(flux_table[14], 10, 0; interpol=true) catch e e end, Exception)
 end
 
